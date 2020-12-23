b39edc86c451be49d14fafe3da6e460e1dc8ef09
gb5256 <324658+gb5256@users.noreply.github.com>
2020-12-23 13:25:14 +0100

    Updated Vendor Files

--- src/assets/datepicker/vendor/js/locales/bootstrap-datepicker.sq.js
+++ src/assets/datepicker/vendor/js/locales/bootstrap-datepicker.sq.js
@@ -1,15 +1,18 @@
 /**
  * Albanian translation for bootstrap-datepicker
- * Tomor Pupovci <http://www.github.com/ttomor>
+ * Tomor Pupovci <https://www.github.com/ttomor>
  */
 ;(function($){
 	$.fn.datepicker.dates['sq'] = {
-		days: ["E Diel", "E Hënë", "E martē", "E mërkurë", "E Enjte", "E Premte", "E Shtunë", "E Diel"],
-		daysShort: ["Die", "Hën", "Mar", "Mër", "Enj", "Pre", "Shtu", "Die"],
-		daysMin: ["Di", "Hë", "Ma", "Më", "En", "Pr", "Sht", "Di"],
+		days: ["E Diel", "E Hënë", "E Martē", "E Mërkurë", "E Enjte", "E Premte", "E Shtunë"],
+		daysShort: ["Die", "Hën", "Mar", "Mër", "Enj", "Pre", "Shtu"],
+		daysMin: ["Di", "Hë", "Ma", "Më", "En", "Pr", "Sht"],
 		months: ["Janar", "Shkurt", "Mars", "Prill", "Maj", "Qershor", "Korrik", "Gusht", "Shtator", "Tetor", "Nëntor", "Dhjetor"],
 		monthsShort: ["Jan", "Shk", "Mar", "Pri", "Maj", "Qer", "Korr", "Gu", "Sht", "Tet", "Nën", "Dhjet"],
-		today: "Sot"
+    monthsTitle: "Muaj",
+		today: "Sot",
+    weekStart: 1,
+    format: "dd/mm/yyyy",
+    clear: "Pastro"
 	};
 }(jQuery));
-
